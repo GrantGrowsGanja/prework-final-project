5ab2fa680a9b84754cf155038b7d52e0b53e2186
Grant Ours <grant.ours55@gmail.com>
2022-05-23 18:10:48 -0600

    made the onclick command switch back and forth between pics

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function changeImage() {
-    document.getElementById("sp1").src="happypeepo1.png";
+    var image = document.getElementById('sp1');
+    if (image.src.match("sadpeepo1.png")) {
+        image.src = "happypeepo1.png";
+    } else {
+        image.src = "sadpeepo1.png";
+    }
 }
 function sp1Hint() {
     alert("Try clicking ;)");
