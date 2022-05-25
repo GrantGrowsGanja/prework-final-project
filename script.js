1a7b8d5389653abd877b2a6b58960c9313884598
Grant Ours <grant.ours55@gmail.com>
2022-05-25 13:51:29 -0600

    added second sadge peepo with hint button

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function changeImage() {
 function sp1Hint() {
     alert("Try clicking ;)");
 }
+function sp2b() {
+    var image2 = document.getElementById('sp2');
+    if (image2.src.match("sadpeepo2.png")) {
+        image2.src = "happypeepo2.png";
+    } else {
+        image2.src = "sadpeepo2.png";
+    }
+}
+function sp2Hint() {
+    alert("Try hovering your mouse over peepo!")
+}
