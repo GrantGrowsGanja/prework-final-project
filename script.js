66bebb1c0c312ff5c8fa69512b81c87f944c82d1
Grant Ours <grant.ours55@gmail.com>
2022-05-25 14:12:25 -0600

    added final sadge peepo with buttons to change the picture

--- script.js
+++ script.js
@@ -20,3 +20,46 @@ function sp2b() {
 function sp2Hint() {
     alert("Try hovering your mouse over peepo!")
 }
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = 'sadpeepo3.png';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'sadpeepo4.png';
+
+imgArray[2] = new Image();
+imgArray[2].src = 'sadpeepo1.png';
+
+imgArray[3] = new Image();
+imgArray[3].src = 'sadpeepo2.png';
+
+imgArray[4] = new Image();
+imgArray[4].src = 'happypeepo3.png';
+
+function nextImage(){
+    var img = document.getElementById("sp3");
+    for(var i = 0; i < imgArray.length;i++){
+        if(imgArray[i].src == img.src){
+            if(i === imgArray.length){
+                document.getElementById("sp3").src = imgArray[0].src;
+                break;
+            }
+            document.getElementById("sp3").src = imgArray[i+1].src;
+            break;
+        }
+    }
+}
+function previousImage(){
+    var img = document.getElementById("sp3");
+    for(var i = imgArray.length-1; i >=0 ;i--){
+        if(imgArray[i].src == img.src){
+            if(i === imgArray.length){
+                document.getElementById("sp3").src = imgArray[4].src;
+                break;
+            }
+            document.getElementById("sp3").src = imgArray[i-1].src;
+            break;
+        }
+    }
+}
